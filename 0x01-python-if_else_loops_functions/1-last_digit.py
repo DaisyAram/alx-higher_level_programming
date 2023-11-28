277ce82238c6082ca85bc8671df2f72771f12e95
DaisyAram <daisyarama03@gmail.com>
2023-11-28 03:15:01 -0800

    modulo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print(chr(i), end = ' ')
-print()
+import random
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+if last_digit > 5:
+    print(f'Last digit of {number} is {last_digit} and is greater than 5')
+elif last_digit == 0:
+    print(f'Last digit of {number} is {last_digit} and is 0')
+else:
+    print(f'Last digit of {number} is {last_digit} and is less than 6 and not 0')
+
