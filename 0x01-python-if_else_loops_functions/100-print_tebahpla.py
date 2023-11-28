6ac73651a19f38b9ca08a5539a59b07d7cd0b528
DaisyAram <daisyarama03@gmail.com>
2023-11-28 09:04:48 -0800

    pycodestyle

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        i = i - 32
-        print("{}".format(chr(i)),end="")
+print(''.join(chr(i) if i % 2 == 0 else chr(i-32) for i in range(122, 96, -1)))
