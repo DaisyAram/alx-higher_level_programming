9ac44fcfd87ae1d493251dac494cad51870b2de9
DaisyAram <daisyarama03@gmail.com>
2023-11-30 04:48:13 -0800

    Easy print

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,1 +1,2 @@
 #!/usr/bin/python3
+str ='#pythoniscool';exec('print(str)')
--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print('ABCDEFGHIJKLMNOPQRSTUVWXYZ', end=' ')
-print()
+from string import ascii_uppercase
+print(ascii_uppercase)
