e4297bb53be16e8e4ea9f30348f7345a0399466d
DaisyAram <daisyarama03@gmail.com>
2023-11-30 09:35:06 -0800

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if _name_=='_main_':
+if __name__ == "__main__":
+    """Print all hidden directories"""
     import hidden_4
-    #print sorted name in the directory
-    for name in sorted(dir(hidden_4)):
-        if name[:2]!='_':
-            print('{}'.format(name))
+
+    for i in dir(hidden_4):
+        if i[:2]!="__":
+            print(i)
