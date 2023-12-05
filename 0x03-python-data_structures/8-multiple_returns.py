38fb1741e0015788849594297661a29cbd72e3e8
DaisyAram <daisyarama03@gmail.com>
2023-12-05 04:12:21 -0800

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def multiple_returns(sentence):
     leng = len(sentence)
     first_char = sentence[0] if leng > 0 else "None"
-    tup = leng,first_char
+    tup = leng, first_char
     return (tup)
