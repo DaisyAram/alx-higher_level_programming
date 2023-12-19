4f89ee790bb36dcf6e1640e949a31f695adb1cd4
DaisyAram <daisyarama03@gmail.com>
2023-12-19 05:18:20 -0800

    main.c

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Square module."""
+"""def Square module."""
 
 
 class Square:
 
-    """Defines a square."""
+    """Represents a square."""
 
     def __init__(self, size=0):
 
