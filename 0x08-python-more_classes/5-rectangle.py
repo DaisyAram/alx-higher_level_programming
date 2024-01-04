429c9b914dc2683c12436da6035344c439eb2909
DaisyAram <daisyarama03@gmail.com>
2024-01-04 07:19:39 -0800

    Detect instance deletion

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""defines a rectangle"""
+
+
+class Rectangle:
+    """rep a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """init a new rectangle
+        args:
+        width(int): width of a rectangle
+        height(int): height of a rectangle
+        """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle."""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ calculate the area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ calculate perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join("#" * self.__width for _ in range(self.__height))
+
+    def __repr__(self):
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
+
+    def __del__(self):
+        """print message"""
+        print("Bye rectangle...")
