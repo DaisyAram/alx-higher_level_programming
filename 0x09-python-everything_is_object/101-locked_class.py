fde1794acdfcf64fccabea39241c4bbeea5df0af
DaisyAram <daisyarama03@gmail.com>
2024-01-05 03:26:25 -0800

    pythonic &low memory cost

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+class LockedClass:
+    def __init__(self):
+        # You can add any instance attribute here.
+        self.first_name = None
+
+    def __setattr__(self, name, value):
+        if name == 'first_name' or hasattr(self, name):
+            super().__setattr__(name, value)
+        else:
+            raise AttributeError("'LockedClass' object has no attribute 'last_name'")
