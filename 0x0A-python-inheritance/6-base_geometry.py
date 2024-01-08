bd4ba9612115177bf6a8cacd7bcadd3c466f0d08
DaisyAram <daisyarama03@gmail.com>
2024-01-08 02:48:35 -0800

    integer validator

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines class BaseGeometry"""
+
+
+class BaseGeometry:
+    """an empty class"""
+    def area(self):
+        """raises exception when called"""
+        raise Exception ("area() is not implemented")
