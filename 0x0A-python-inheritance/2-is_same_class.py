fc3bead23f770c724fc718389dfd8fff04a2ec4e
DaisyAram <daisyarama03@gmail.com>
2024-01-08 04:12:37 -0800

    exact same ibject

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,6 @@
 
 
 def is_same_class(obj, a_class):
-    """returns True if the object is exactly
+    """returns True if the obj is exactly
     an instance of the specified class ; otherwise False."""
-    return(type(obj) == a_class)
+    return (type(obj) == a_class)
