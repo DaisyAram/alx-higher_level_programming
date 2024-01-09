0b8ad38621c7263f0d011b9715e3a78e9319bb6a
DaisyAram <daisyarama03@gmail.com>
2024-01-09 03:22:04 -0800

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,6 +17,6 @@ class Student:
         for a in attrs:
             try:
                 new_dict[a] = self.__dict__[a]
-            except:
+            except KeyError:
                 pass
         return new_dict
