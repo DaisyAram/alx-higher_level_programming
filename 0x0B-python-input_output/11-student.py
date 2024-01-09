51320753afb41bee425d77078d00a51e3f2cbd44
DaisyAram <daisyarama03@gmail.com>
2024-01-09 03:29:05 -0800

    replaces all attributes

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""class that defines a student"""
+
+
+class Student:
+    """rep of a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dic rep of a student instance with specified attrs"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for a in attrs:
+            try:
+                new_dict[a] = self.__dict__[a]
+            except KeyError:
+                pass
+        return new_dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key in json:
+            try:
+                setattr(self, key, json[key])
+            except KeyError:
+                pass
