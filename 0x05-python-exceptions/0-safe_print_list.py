59041570be953124c7f9500709677afd339ca655
DaisyAram <daisyarama03@gmail.com>
2024-01-17 01:59:13 -0800

    safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            num = num + 1
-        except IndexError:
-            break
-        print("")
-        return
+    real_elements_printed = 0
+    try:
+        for i in range(len(my_list)):
+            print(my_list[i], end=" ")
+            real_elements_printed += 1
+            if real_elements_printed == x:
+                break
+        print()
+    except TypeError:
+        print("Error: List must contain only printable elements")
+    except Exception as e:
+        print("Error: ", str(e))
+
+    return real_elements_printed
