85f54acaa3f48c9ab6c72516035fbc0555260734
DaisyAram <daisyarama03@gmail.com>
2024-02-13 01:46:08 -0800

    first add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- Creates a table called first_table in the current database in your MySQL server.
-CREATE TABLE IF NOT EXISTS first_table(id INT,
-	name VARCHAR(256));
+--  inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+INSERT INTO first_table(id, name)
+VALUES(89, 'Best School');
