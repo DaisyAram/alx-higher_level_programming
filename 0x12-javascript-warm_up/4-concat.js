0fb84bf37270818b8d2aa7a72d6e3071fb3368c2
DaisyAram <daisyarama03@gmail.com>
2024-03-11 02:19:47 -0700

    args

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-if (process.argv.length === 3) {
-  console.log('c is undefined');
-} else if (process.argv.length === 4) {
-  console.log(`${process.argv[2]} is ${process.argv[3]}`);
-} else {
-  console.log('undefined is undefined');
-}
+console.log(process.argv[2] + 'is' + process.argv[3]);
