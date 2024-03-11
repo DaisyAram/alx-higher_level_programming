9fa0da9b2a44518018020cfff83ff042ca00cb02
DaisyAram <daisyarama03@gmail.com>
2024-03-11 03:10:19 -0700

    args

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+  const i = Number(process.argv[2]);
+  let x = 0;
+  while (x < i) {
+    console.log('C is fun');
+    x++;
+  }
+}
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Missing size');
+} else {
+  const i = Number(process.argv[2]);
+  let x = 0;
+  while (x < i) {
+    console.log('X'.repeat(i));
+    x++;
+  }
+}
