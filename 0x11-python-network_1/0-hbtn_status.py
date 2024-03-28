afbf4537431ce0e07f3dc535a47530bacefc74f0
DaisyAram <daisyarama03@gmail.com>
2024-03-28 02:29:19 -0700

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-""" python script that fetches
-https://alx-intranet.hbtn.io/status using urllib
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
 
-from urllib import request
 
-if __name__ == "__main__":
-    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        print(f'Body response:')
-        print(f'    - type: <\'{type(html)}\'')
-        print(f'    - content: {repr(html)}')
-        print(f'    - utf8 content: {html.decode()}')
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
