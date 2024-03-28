0576590b0939a2e190fec89af4080854ad669ff0
DaisyAram <daisyarama03@gmail.com>
2024-03-28 02:16:18 -0700

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-# python script that fetches https://alx-intranet.hbtn.io/status using urllib
+"""python script that fetches https://alx-intranet.hbtn.io/status using urllib"""
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-    print(f'Body response:')
-    print(f'    - type: <\'{type(html)}\'')
-    print(f'    - content: {repr(html)}')
-    print(f'    - utf8 content: {html.decode()}')
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print(f'Body response:')
+        print(f'    - type: <\'{type(html)}\'')
+        print(f'    - content: {repr(html)}')
+        print(f'    - utf8 content: {html.decode()}')
