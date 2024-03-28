6c7d714f1d9b03735f63c9fa1a69509a3d8e377c
DaisyAram <daisyarama03@gmail.com>
2024-03-28 04:23:35 -0700

    network

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""A script tha:
-- takes in a letter
-- sends POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter.
+"""a Python script that takes in a letter and sends a POST
+- request to http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
+
+
 import sys
 import requests
 
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = r.json()
+        response = req.json()
         if response == {}:
             print("No result")
         else:
