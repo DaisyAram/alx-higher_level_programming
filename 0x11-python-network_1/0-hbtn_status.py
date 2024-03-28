2bad8dcae97a7a4ec953d8f3083797653a383290
DaisyAram <daisyarama03@gmail.com>
2024-03-28 01:44:30 -0700

    Response header value #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#python script that fetches https://alx-intranet.hbtn.io/status using urllib
+# python script that fetches https://alx-intranet.hbtn.io/status using urllib
 import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
