0bd22075b3bc7facea2324aa4459e57c6a4fca81
DaisyAram <daisyarama03@gmail.com>
2024-04-23 02:35:38 -0700

    writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,10 +3,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-    return;
+fs.writeFile(filePath, content, error => {
+  if (error) {
+    console.log(error);
   }
-  console.log(content);
 });
