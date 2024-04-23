a8d3c32cfaae3a81d4757135bc2987a23d850ea2
DaisyAram <daisyarama03@gmail.com>
2024-04-23 02:10:12 -0700

    readme

---
+++ 0x14-javascript-web_scraping/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
