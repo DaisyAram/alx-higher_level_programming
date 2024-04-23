514c615554ce61136ead7ef1fcdb59f7ee1276e1
DaisyAram <daisyarama03@gmail.com>
2024-04-23 03:35:01 -0700

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(`An error occurred: ${error}`);
+    process.exit(1);
+  }
+
+  const data = JSON.parse(body);
+
+  const moviesWithWedge = data.results.filter((movie) => {
+    return movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/');
+  });
+
+  console.log(moviesWithWedge.length);
+});
